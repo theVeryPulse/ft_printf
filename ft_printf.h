3dc025b7721ad867d7d626095000e456d27c63b9
Philip Li 42 <macpilot23@outlook.com>
2023-11-16 22:07:39 +0000

    Create ft_printf.h
    Alternative name for libftprintf.h

---
+++ ft_printf.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Philip Li <LJHR.UK@outlook.com>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/10 19:17:08 by Philip Li         #+#    #+#             */
+/*   Updated: 2023/11/16 22:06:02 by juli             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+# include "libft/libft.h"
+# include <stdarg.h>
+
+int		ft_printf(const char *str, ...);
+
+int		ft_printf_all(va_list args, int str_i);
+
+int		ft_printf_c(char c);
+
+int		ft_printf_s(char *s);
+
+int		ft_printf_p(void *ptr);
+
+int		ft_printf_d(int num);
+
+int		ft_printf_u(unsigned int n);
+
+int		ft_printf_x(int n, int x);
+
+void	str_rev(char *s);
+
+int		ft_abs(int n);
+
+#endif
